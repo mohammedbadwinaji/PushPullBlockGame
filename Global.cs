03f8f5b56a2dfbc1012b97e07a279dfce1a7910d
mohammedbadwinaji <167030088+mohammedbadwinaji@users.noreply.github.com>
2026-03-16 03:11:49 +0300

    Update BoardsFilePath to a generic path

--- Global.cs
+++ Global.cs
@@ -5,7 +5,7 @@ namespace PushPullBlocksGame
 
     public static class Global
     {
-        public static string BoardsFilePath = @"C:\Users\akats\source\repos\PushPullBlocksGame\Data\boardsData.json";
+        public static string BoardsFilePath = "path of boards data file";
         public static int LevelsCount = 42;
         public static int NumberOfLevelsPerLine = 7;
         public static List<Board> Boards = null;
