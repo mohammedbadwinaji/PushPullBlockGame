28b42deb338ebb77b56df48e75041aafaee72ded
mohammedbadwinaji <167030088+mohammedbadwinaji@users.noreply.github.com>
2026-03-16 03:13:51 +0300

    Remove Person class and PrintActionPath method
    Removed the Person class and the PrintActionPath method from the Program class.

--- Program.cs
+++ Program.cs
@@ -6,45 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Collections;
-public class Person
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-   
-}
+
 namespace PushPullBlocksGame
 {
-    
     internal class Program
     {
-
-    
-        public static void PrintActionPath(State state)
-        {
-            if (state == null) {
-                Console.WriteLine("\t\t\t\tNo Path");
-                return;
-            };
-
-            Stack<State> stack = new Stack<State>();
-
-            while (state != null)
-            {
-                stack.Push(state);
-                state = state.parent;
-            }
-
-            int Counter = 1;
-            Console.WriteLine($"\t\t\t\tPath Length = {stack.Count - 1}");
-            return;
-            Console.WriteLine("\t\t\t\tActions : ");
-            foreach (State st in stack)
-            {
-                if (st.Action == null) continue;
-                Console.WriteLine($"\t\t\t\t[ {Counter} ] : {st.Action} ");
-                Counter++;
-            }
-        }
         static void Main(string[] args)
         {
             Game.Open();
